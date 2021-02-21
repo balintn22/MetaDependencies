52f2c467f068517c83a50dabe3448e6353bb1eaf
balintn <balintn@hotmail.com>
2021-02-21 12:20:22 +0000

    Added usage info.

--- MetaDependencies/AssembliesByRepo/Program.cs
+++ MetaDependencies/AssembliesByRepo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using AssembliesByRepo.Logic;
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace AssembliesByRepo
 {
@@ -7,6 +9,14 @@ namespace AssembliesByRepo
     {
         static void Main(string[] args)
         {
+            var helpArgs = new string[] { "help", "-help", "/help" };
+
+            if (args.Any(x => helpArgs.Contains(x.ToLower())))
+            {
+                ShowUsage();
+                return;
+            }
+
             ICrawler crawler = new Crawler();
             Console.WriteLine(
                 $"AssemblyName\tType\tPath" +
@@ -21,6 +31,26 @@ namespace AssembliesByRepo
                     $"\t{pi.GeneratePackage}\t{pi.PackageId}\t{pi.RepositoryUrl}" +
                     $"\t{pi.Description}\t{pi.TargetFrameworks}");
             }
+        }
+
+        private static void ShowUsage()
+        {
+            string appName = Assembly.GetExecutingAssembly().GetName().Name;
+
+            Console.WriteLine(
+                $"{appName} crawls all subdirectories and find .csproj files. " +
+                "It'll then output information about those assemblies, including " +
+                "build output type (exe/dll), nuget packaging properties, " +
+                "assembly description and target frameworks, etc.\n" +
+                "it supports old style (VS2017 and before) and SDK style .csproj files.\n" +
+                "\n" +
+                "Usage:\n" +
+                $"   Add {appName} to yuor path, or copy {appName} and {appName}.Logic to the folder where you start your search.\n" +
+                "   To display assembly info on screen:\n" +
+                $"      {appName}\n" +
+                "   To write assembly info to a text file:\n" +
+                $"      {appName} > out.txt\n"
+            );
         }
     }
 }
